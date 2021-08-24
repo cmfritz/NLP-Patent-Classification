aadef5b06ce7b1e46a9ded10ee3fd7e7337c6928
cmfritz <cmfritz0@gmail.com>
2021-08-24 18:54:02 -0500

    Update notebook and readme

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# capstone
+# Using NLP to predict patent classification
+
+## Business Understanding
+Patents provide a wealth of data since their data has been compiled by a variety of parties in a variety of ways, both public and private. To name a few, public patent data can be accessed via websites for the U.S. Patent & Trademark Office (USPTO), foreign patent offices, Google Patents, etc. Data for this project has been obtained from [PatentsView.org](PatentsView.org), which is a visualization, data dissemination, and analysis platform provided by the USPTO.
+
+Various types of information on patents are connected at various stages in a patent's life, one being the classification of a patent's technology (e.g. mechanical, chemistry, electrical, etc.) This classification is assigned some time after a patent is filed, but it could be useful to try and predict how a patent will be classified before it is filed. In this project, I will be attempting to use machine learning to predict classifications. There are many types of classification systems ranging from broad to very specific, so for the purposes of this project, I will focus on a broader classification system.
+
+## Data Understanding
+To provide some background on how a patent is structured, there are 4 main parts. They are:
+1) Coverpage, which provides bibliographic information such as who the inventors are, what the title is, a summary, what patents are related, etc.; 
+2) Specification, which provides background information on the given invention;
+3) Drawings, which serve to illustrate the invention; and
+4) Claims, which detail the specific aspects of an invention that the applicant wants legally protected. 
+
+Here is a link to patent [#10 million](https://patentimages.storage.googleapis.com/c0/d5/f7/86ad5b42759506/US10000000.pdf) if you are curious what the actual document looks like.
+
+I mention these because in this project, we will use the first claim of U.S. patents to predict the classification. Claims are much shorter and more specific than the entire specification (a few lines vs. many page) and follow a similar grammatical structure. This structure is a product of patent attorneys seeking to obtain specific and predictable legal protections, and may help in pattern recognition. Alternatively, the styles of the specifications are highly personalized to the author of the patent since the rules for the contents are not as rigid. Lastly, since each patent is required to contain at least one claim, we are guaranteed that there will not be missing data for any given patent we are analyzing. 
+
+## Data Preparation
+Currently, there have been over 10 million patents granted in the U.S., so the first thing to do is reduce the amount of data being used for modeling since I am limited to what Google Collab can run in a couple hours. The parameters I used to select the data for training are:
+1) Patents from the last 10 years. This provides more than 3 million patents for further trimming.
+2) Using the World Intellectual Property Organization's (WIPO) classifications, found [here](https://patentsview.org/download/data-download-tables), which are the broadest having only 5 classifications:
+    - Electrical engineering
+    - Mechanical engineering
+    - Instruments
+    - Chemistry 
+    - Other (which patents I eliminated since I wanted to focus on data with an actual label)
+3) Getting rid of data having more than one classification
+The first claim is typically considered the most important. A smaller amount of data should reduce the computing power required.
+4) Keeping only the first claim for each patent, which is usually the most important claim of a patent.
+
+## Modeling & Evaluation
+I wanted to look at how a few different sets of models and also compare how they would perform in dual or multiclass situations. 
+
+### Multiclass
+#### Model 1 - Logistic Regression
+For my first model, I used logistic regression, and below are the results from the model.
+
+Training Accuracy: 62.67%<br />
+Test Accuracy: 62.71%
+
+Confusion Matrix for test data:<br />
+![pic1](./images/cm_logistic_regression_4class.png)
+
+#### Model 2 - Decision Tree
+For my second model, I used a decision tree classifier. Below are the results.
+
+Training Accuracy: 96.12%<br />
+Test Accuracy: 58.58%
+
+Confusion Matrix for test data:<br />
+![pic2](./images/cm_decision_tree_4class.png)
+
+#### Model 3 - XG Boost
+#### Model 4 - Nearest Neighbors
+
+### Dualclass
+#### Model 1 - Logistic Regression
+#### Model 2 - Decision Tree
+#### Model 3 - XG Boost
+#### Model 4 - Nearest Neighbors
+#### Model 5 - Deep Learning
+
+## Evaluation
+
+## Conclusion
+### Results
+### Recommendations
+
+## Deployment
+For More Information, please review my full analysis in Jupyter Notebook or my presentation.
+
+For any additional questions, please contact Catherine Fritz: cmfritz0@gmail.com.
+
+## Repository Structure
--- mylibrary.py
+++ mylibrary.py
@@ -1,9 +1,131 @@
-# Module to preprocess my data
-
-def download_file(url):
-    import gc
-    data = pd.read_csv(url, compression='zip', sep='\t')
-    df = pd.DataFrame(data)
-    del data
-    gc.collect()
+# Module of custom functions
+#################################################################
+def tsv_to_df(url):
+    from pandas import read_csv
+    df = read_csv(url, compression='zip', sep='\t', dtype=str)
     return df
+    
+#################################################################   
+def merge_and_clean(df1, df2):
+    # counts = df1.patent_id.value_counts()
+    # counts = counts.to_frame()
+    # counts = counts.reset_index()
+    # groups = df_pats_w_sectors.groupby(by='patent_id').count()
+    df1 = df1.drop(['field_title'], axis=1)
+    df1 = df1.drop_duplicates()
+    groups = df1.groupby(by='patent_id').count()
+    singles = groups[groups.sector_title == 1]
+    singles = singles.reset_index()
+    ok_pats = df1.merge(singles, on='patent_id')
+    ok_pats = ok_pats.drop(['sector_title_y'], axis=1)
+    ok_pats = ok_pats.rename(columns={'sector_title_x':'sector'})
+    ok_pats.patent_id = ok_pats.patent_id.astype('int64')
+    df_merged = ok_pats.merge(df2, how='inner', on='patent_id')
+    return df_merged
+    
+#################################################################
+def train_and_predict(model, X_train, X_test, y_train):
+    '''
+    Given a model and train and test data, function fits the model and returns predicted values.
+    
+    Parameters: 
+    model: object containing initialized model for training
+    Xy: object containing train and test data for X and y sets
+    
+    Returns:
+    Xy: updated Xy object with X & y prediction data (y_hat_train and y_hat_test)
+    
+    '''
+    model.fit(X_train, y_train)
+    
+    # Make predictions for test data
+    y_hat_train = model.predict(X_train)
+    
+    # Make predictions for test data
+    y_hat_test = model.predict(X_test)
+    
+    return y_hat_train, y_hat_test
+
+#################################################################   
+def model_scores(model, model_name,
+                 X_train, X_test, 
+                 y_train, y_test):
+    '''
+    Takes model and test train data objects and generates scores and confusion matrix.
+    
+    Parameters: 
+    model: object containing initialized model for training
+    Xy: object containing train and test data for X and y sets
+    
+    Returns:
+    printed text and visualizations
+    '''
+
+    from sklearn.metrics import accuracy_score, f1_score, recall_score
+    from sklearn.metrics import plot_confusion_matrix
+    from pandas import DataFrame
+    from matplotlib.pyplot import savefig, title
+
+    y_hat_train, y_hat_test = train_and_predict(model, X_train, X_test, y_train)
+
+    scores = {}
+
+    # Caclulate accuracy for test data= 
+    scores['train_accuracy'] = accuracy_score(y_train, y_hat_train)
+
+    # Caclulate accuracy for test data
+    scores['test_accuracy'] = accuracy_score(y_test, y_hat_test)
+    
+    no_classes = len(set(y_train))
+
+    if no_classes < 3:
+        # Caclulate recall for train data
+        scores['train_recall'] = recall_score(y_train, y_hat_train, pos_label='Instruments')
+    
+        # Calculate recall for test data
+        scores['test_recall'] = recall_score(y_test, y_hat_test, pos_label='Instruments')
+    
+        # Caclulate F1 for train data
+        scores['train_f1'] = f1_score(y_train, y_hat_train, pos_label='Instruments')
+    
+        # Calculate F1 for test data
+        scores['test_f1'] = f1_score(y_test, y_hat_test, pos_label='Instruments')
+
+    # Plot confusion matrix
+    plot_confusion_matrix(model, X=X_test, y_true=y_test,
+                          values_format = 'd', xticks_rotation='vertical')
+    title(f'{model_name} - Test Data Confusion Matrix')
+    savefig(f'images/cm_{model_name}_{no_classes}class.png', bbox_inches='tight', dpi=300)
+    
+    return DataFrame(scores, index=[model_name])
+    
+    #################################################################  
+def run_grid_search(params, model, X_train, y_train):
+    '''
+    Takes in parameters, model, and test train data, runs grid search, and 
+    returns updated model and test train data.
+    
+    Parameters: 
+    params: dict with parameters for grid search
+    model: object containing initialized model for training
+    Xy: object containing train and test data for X and y sets
+    
+    Returns:
+    gs: object containing updated model
+    Xy: object with test train data
+    '''
+    gs = GridSearchCV(model, params, scoring='f1_macro', cv=None, n_jobs=1)
+    gs = gs.fit(X_train, y_train)
+    best_parameters = gs.best_params_
+
+    print('Grid Search found the following optimal parameters: ')
+    for param_name in sorted(best_parameters.keys()):
+        print('%s: %r' % (param_name, best_parameters[param_name]))
+
+    # Make predictions for test data
+    y_hat_train = gs.predict(X_train)
+    
+    # Make predictions for test data
+    y_hat_test = gs.predict(X_test)
+    
+    return gs, y_hat_train, y_hat_test
